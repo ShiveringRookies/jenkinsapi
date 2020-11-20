ccfb10e82b54fffe11dc8200100d4efb7fe785cd
xiaoboya <xby@Gerrit123>
2020-11-20 14:13:09 +0800

    feat:add function to get index info

--- struct_index.go
+++ struct_index.go
@@ -3,3 +3,27 @@ package jenkinsapi
 type AssignedLabel struct {
 	Name string `json:"name" xml:"name"`
 }
+
+type BaseStruct struct {
+	Class string `json:"_class"`
+}
+
+type IndexInfo struct {
+	Class           string          `json:"_class"`
+	AssignedLabels  []AssignedLabel `json:"assignedLabels" xml:"assignedLabels"`
+	Mode            string          `json:"mode" xml:"mode"`
+	NodeDescription string          `json:"nodeDescription" xml:"nodeDescription"`
+	NodeName        string          `json:"nodeName" xml:"nodeName"`
+	NumExecutors    int             `json:"numExecutors" xml:"numExecutors"`
+	Description     interface{}     `json:"description" xml:"description"`
+	Jobs            []JenkinsJob    `json:"jobs"`
+	OverallLoad     interface{}     `json:"overallLoad" xml:"overallLoad"`
+	PrimaryView     JenkinsView     `json:"primaryView" xml:"primaryView"`
+	QuietingDown    bool            `json:"quietingDown" xml:"quietingDown"`
+	SlaveAgentPort  int             `json:"slaveAgentPort" xml:"slaveAgentPort"`
+	UnlabeledLoad   BaseStruct      `json:"unlabeledLoad" xml:"unlabeledLoad"`
+	URL             string          `json:"url" xml:"url"`
+	UseCrumbs       bool            `json:"useCrumbs" xml:"useCrumbs"`
+	UseSecurity     bool            `json:"useSecurity" xml:"useSecurity"`
+	Views           []JenkinsView   `json:"views"`
+}
--- urls.go
+++ urls.go
@@ -1,6 +1,7 @@
 package jenkinsapi
 
 var (
+	IndexJsonURL    = "/api/json"
 	UserInfoJsonURL = "/user/$username/api/json"
 	UserListJsonURL = "/asynchPeople/api/json"
 )
