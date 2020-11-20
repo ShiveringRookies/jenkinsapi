24e30fec2d6ad370a0998ebbb187e8739ee583c5
xiaoboya <xby@Gerrit123>
2020-11-20 13:27:24 +0800

    fix:add function for show user's info

--- show_user.go
+++ show_user.go
@@ -11,19 +11,54 @@ type User struct {
 	FullName    string `json:"fullName" xml:"fullName"`
 }
 
-type UserInfo struct {
+type UserSimpleInfo struct {
 	LastChange interface{} `json:"lastChange" xml:"-"`
 	Project    interface{} `json:"project" xml:"-"`
 	User       User        `json:"user" xml:"user"`
 }
 
 type UserList struct {
-	Class string     `json:"_class"`
-	Users []UserInfo `json:"users"`
+	Class string           `json:"_class"`
+	Users []UserSimpleInfo `json:"users"`
 }
 
 func (j *JenkinsClient) GetUserListJson() (userList *UserList, err error) {
 	req, err := http.NewRequest("GET", j.Addr+"/asynchPeople/api/json", nil)
+	if err != nil {
+		return nil, err
+	}
+	j.SetAuth(req)
+	client := &http.Client{}
+	response, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+	resp, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+	err = json.Unmarshal(resp, &userList)
+	return userList, err
+}
+
+type UserProperty struct {
+	Class             string `json:"_class"`
+	Address           string `json:"address,omitempty"`
+	InsensitiveSearch bool   `json:"insensitiveSearch,omitempty"`
+}
+
+type UserInfo struct {
+	Class       string         `json:"_class"`
+	AbsoluteURL string         `json:"absoluteUrl" xml:"absoluteUrl"`
+	ID          string         `json:"id" xml:"id"`
+	Description string         `json:"description" xml:"description"`
+	FullName    string         `json:"fullName" xml:"fullName"`
+	Property    []UserProperty `json:"property"`
+}
+
+func (j *JenkinsClient) GetUserInfoJson(username string) (userList *UserInfo, err error) {
+	req, err := http.NewRequest("GET", j.Addr+"/user/"+username+"/api/json", nil)
 	if err != nil {
 		return nil, err
 	}
--- show_user_test.go
+++ show_user_test.go
@@ -18,3 +18,20 @@ func TestJenkinsClient_GetUserListJson(t *testing.T) {
 		t.Log(userList)
 	}
 }
+
+func TestJenkinsClient_GetUserInfoJson(t *testing.T) {
+	j := JenkinsClient{
+		Addr: "http://127.0.0.1:8080",
+		BasicAuth: BasicAuth{
+			UserName: "xiaoboya",
+			Password: "xby951111",
+		},
+		BearerAuth: "",
+	}
+	userList, err := j.GetUserInfoJson("xiaoboya")
+	if err != nil {
+		t.Error(err)
+	} else {
+		t.Log(userList)
+	}
+}
